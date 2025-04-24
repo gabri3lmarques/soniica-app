65b3cae617bdb48bcfb8de092b4f5f414bbec1f3
gabri3lmarques <gabri3lmarques@gmail.com>
2025-04-24 11:05:08 -0300

    Building the album page front-end

--- category-artist-album.php
+++ category-artist-album.php
@@ -1,9 +1,14 @@
 <?php
 if ( is_category() ) {
-
     // Recupera o objeto da categoria atual
     $current_category = get_queried_object();
-
+    if ( ! empty( $current_category ) && ! is_wp_error( $current_category ) ) {
+        // Obtém o ID da imagem salva como meta da categoria
+        $image_id = get_term_meta( $current_category->term_id, 'category-image', true );
+        $image_url = $image_id ? wp_get_attachment_url( $image_id ) : '';
+    }
+    echo($current_category->name);
+    echo($image_url);
     // Configura os argumentos para a query
     $args = array(
         'post_type'      => 'song', // Tipo de post customizado
@@ -16,10 +21,8 @@ if ( is_category() ) {
             ),
         ),
     );
-
     // Realiza a query personalizada
     $song_query = new WP_Query( $args );
-
     // Verifica se há posts para exibir
     if ( $song_query->have_posts() ) {
         echo '<ul>';
@@ -37,7 +40,6 @@ if ( is_category() ) {
     } else {
         echo '<p>Nenhum post do tipo "song" encontrado nesta categoria.</p>';
     }
-
     // Restaura os dados originais do post
     wp_reset_postdata();
 }

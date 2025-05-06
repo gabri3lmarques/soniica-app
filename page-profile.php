6f29073b9231bfb43627098ce4951b573ae1dff3
gabri3lmarques <gabri3lmarques@gmail.com>
2025-05-06 18:03:53 -0300

    Profile page front-end

--- page-profile.php
+++ page-profile.php
@@ -1,12 +1,11 @@
-
-
-
 <?php
+use svg\SvgIcons;
 get_header();
 require_once get_template_directory() . '/components/search/Search.php';
 $user_id = get_current_user_id();
 $profile_url = get_user_meta($user_id, 'profile_picture_url', true);
 ?>
+
 <div class="top-bar">
     <?php include 'components/top-menu/top-menu.php'; ?>
 </div>
@@ -17,13 +16,27 @@ $profile_url = get_user_meta($user_id, 'profile_picture_url', true);
     </div>
     <!-- o corpo do site -->
     <div class="main profile">
+        <h2>Account</h2>
         <?php 
             $search = new SearchComponent();
             echo $search->render();
         ?>
+        <div class="profile-img">
+            <?php
+                if($profile_url) {
+                    ?>
+                    <img class="profile-image" src="<?php echo esc_url($profile_url); ?>">
+                    <?php
+                } else {
+                    SvgIcons::render('user');
+                }
+            ?>
+        </div>
         <form id="profile-picture-form" enctype="multipart/form-data">
-          <input type="file" name="profile_picture" accept="image/*" />
-          <button type="submit">Enviar imagem</button>
+            <div class="file-picker-div">
+                <input class="file-picker" id="profile_picture" type="file" name="profile_picture" accept="image/*" />
+            </div>
+            <button type="submit">Enviar imagem</button>
         </form>
 
         <div id="upload-status"></div>

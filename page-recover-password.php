89d73a982c1ce8dec4e79b7ca670e947b473ec57
gabri3lmarques <gabri3lmarques@gmail.com>
2025-06-14 15:08:35 -0300

    Rest Password feature

--- page-recover-password.php
+++ page-recover-password.php
@@ -12,20 +12,33 @@ get_header();
         <div class="page-template_right-side_content">
             <h2>Recuperar senha</h2>
             <p>Digite o seu email no campo abaixo.</p>
-            <p></p>
+            <p>Se houver uma conta relacionada ao seu email você recebra um link de redefinição de senha.</p>
             <form id="recover-form">
-            <label>Email:</label>
-            <input type="email" name="email" required>
-            <button type="submit">Recuperar senha</button>
+                <input type="email" name="email" required>
+                <button type="submit">Recuperar senha</button>
             </form>
             <p id="feedback"></p>
         </div>
     </div>
 </div>
+<div class="playlist-modal-background">
+    <div class="playlist-modal">
+        <div class="playlist-modal-close">
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20">
+                <style>.a{fill:#fff}</style>
+                <path fill-rule="evenodd" class="a" d="m0.4 0.4c0.6-0.5 1.5-0.5 2.1 0l7.5 7.6 7.5-7.6c0.6-0.5 1.5-0.5 2.1 0 0.5 0.6 0.5 1.5 0 2.1l-7.6 7.5 7.6 7.5c0.5 0.6 0.5 1.5 0 2.1-0.6 0.5-1.5 0.5-2.1 0l-7.5-7.6-7.5 7.6c-0.6 0.5-1.5 0.5-2.1 0-0.5-0.6-0.5-1.5 0-2.1l7.6-7.5-7.6-7.5c-0.5-0.6-0.5-1.5 0-2.1z"></path>
+            </svg>                  
+        </div>
+        <div class="playlist-modal-head">
+            <h3>Selecione uma playlist</h3>
+        </div>
+        <div class="playlist-modal-body"></div>
+    </div>
+</div>
 <script type="text/javascript"
         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
 </script>
-<script type="module">
+<script>
 
   emailjs.init('user_IcEtHdn5JVnYwQuQCggEy'); // substitua pelo seu user_id
 
@@ -50,8 +63,17 @@ get_header();
       });
     }
 
-    document.getElementById('feedback').textContent = data.message;
+    //document.getElementById('feedback').textContent = data.message;
+    document.querySelector('.playlist-modal-body').textContent = data.message;
+    document.querySelector('.playlist-modal-background').classList.toggle('active');
+    
   });
+</script>
+<script>
+    const modalCloseButton = document.querySelector('.playlist-modal-close');
+    modalCloseButton.addEventListener('click', function() {
+        document.querySelector('.playlist-modal-background').classList.remove('active');
+    }); 
 </script>
 <?php get_footer(); ?>
 

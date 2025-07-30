a0bb606b94a4d2cd32a3c24ea35393b7d9047358
gabri3lmarques <gabri3lmarques@gmail.com>
2025-07-30 19:52:31 -0300

    Autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabi/sonica-app',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '74fad039d8481a53c485dc85f8523022e0c7158b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabi/sonica-app' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '74fad039d8481a53c485dc85f8523022e0c7158b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v16.6.0',
+            'version' => '16.6.0.0',
+            'reference' => 'd6de0a536f00b5c5c74f36b8f4d0d93b035499ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

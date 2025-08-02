8840d2bd3b8c5cb32343369d867e8450a5c1e316
gabri3lmarques <gabri3lmarques@gmail.com>
2025-08-02 12:03:54 -0300

    Updating coming soon

--- page-casa.php
+++ page-casa.php
@@ -3,35 +3,6 @@
 Template Name: Login Personalizado
 */
 
-use user\Users;
-
-// Verifica se o usuário já está logado
-if ( is_user_logged_in() ) {
-    wp_redirect( home_url() );
-    exit;
-}
-
-$login_error = '';
-
-if ( isset( $_POST['submit'] ) ) {
-    $creds = array();
-    $creds['user_login']    = sanitize_user( $_POST['username'] );
-    $creds['user_password'] = $_POST['password'];
-    $creds['remember']      = isset( $_POST['remember'] );
-
-    $user = wp_signon( $creds, false );
-
-    if ( is_wp_error( $user ) ) {
-        $login_error = $user->get_error_message();
-    } else {
-        // Força verificação de status premium imediatamente após login
-        Users::check_user_premium_status();
-
-        // Redireciona para a home
-        wp_redirect( home_url() );
-        exit;
-    }
-}
 get_header();
 ?>
 <div class="top-bar">
